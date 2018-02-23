360da53e200bb80f3c7648903f9156fb8ace7017
Zhen Liu <liuzhen@touchjet.com>
2018-02-23 19:58:43 +1300

    Check in code.

---
+++ DateTimeFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeUtilities
+{
+    public static class DateTimeFactory
+    {
+        public static DateTime FromUnixTimeStamp(double timestamp)
+        {
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(timestamp);
+            return dtDateTime;
+        }
+    }
+}
